15a6d7016bca7579cd3a14b8f416294cd97af115
Tabea Frenzel <tabea.frenzel@web.de>
2022-08-12 20:27:30 +0200

    [main] Add Vuetify and Fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCartShopping, faPlus, faLocationDot, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.config.productionTip = false
+
+library.add(faCartShopping, faPlus, faLocationDot, faMagnifyingGlass)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
