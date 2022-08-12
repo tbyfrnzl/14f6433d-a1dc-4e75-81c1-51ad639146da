c6a7f5f8cfe7f9fd0231feaa2eae425f614eeba0
Tabea Frenzel <tabea.frenzel@web.de>
2022-08-12 20:56:55 +0200

    [main] Create basic header component, non functional yet

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar app color="primary" dark elevation="2">
+        <v-text-field
+            v-model="search" @input="$emit('search-events', search)"
+            placeholder="Events durchsuchen"
+            dense hide-details filled>
+            <!-- <font-awesome-icon icon="magnifying-glass" size="sm" slot="prepend-inner" /> -->
+        </v-text-field>
+
+        <v-spacer></v-spacer>
+
+        <!-- https://vuetifyjs.com/en/components/badges/ -->
+        <v-badge :content="itemCount" :value="itemCount" color="red">
+            <font-awesome-icon size="lg" icon="cart-shopping"></font-awesome-icon>
+        </v-badge>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: ['itemCount'],
+  data () {
+    return {
+      search: ''
+    }
+  }
+}
+</script>
