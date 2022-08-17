f19b2ca5e4e55f75cf29430b60d00567f8f67844
Tabea Frenzel <tabea.frenzel@web.de>
2022-08-17 18:00:16 +0200

    [main] Add loading indicator

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -2,13 +2,17 @@
     <v-container app>
         <h1 class="mb-5">Public Events</h1>
         <section v-for="(day) in events" :key="day.day">
-            <h2 class="mb-3 mt-3 event-section__header" >{{getDateString(day.day).toUpperCase()}}</h2>
+            <h2 class="mb-3 mt-3 event-list__section-header" >{{getDateString(day.day).toUpperCase()}}</h2>
             <v-row>
                 <v-col cols="4" v-for="(event) in day.events" :key="event._id">
                   <Event :event="event.details" />
                 </v-col>
             </v-row>
         </section>
+        <div v-if="events.length === 0" class="event-list__loading-indicator">
+          <font-awesome-icon icon="spinner" class="fa-spin" />
+          Loading events
+        </div>
     </v-container>
 </template>
 
@@ -43,11 +47,18 @@ export default {
 </script>
 
 <style lang="less">
-.event-section__header {
+.event-list {
+  &__loading-indicator {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+
+  &__section-header {
   position: sticky;
   position: -webkit-sticky;
   top: 64px;
   z-index: 2;
-  background-color: rgba(255, 255, 255, 0.795);
+  background-color: rgba(255, 255, 255, 0.8);
+}
 }
 </style>
