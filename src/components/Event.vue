1923c7dd3d68504fc737b08e3f1637af4f100ed1
Tabea Frenzel <tabea.frenzel@web.de>
2022-08-21 16:18:19 +0200

    [main] Remove extra whitespace in date

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -49,8 +49,7 @@ export default {
       const minutes = String(date.getMinutes()).padStart(2, '0')
       const seconds = String(date.getSeconds()).padStart(2, '0')
 
-      return `${date.getDate()}.${date.getMonth() + 1}.
-        ${date.getFullYear()}, ${date.getHours()}:${minutes}:${seconds}`
+      return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}, ${date.getHours()}:${minutes}:${seconds}`
     },
     addToCart (event) {
       this.cartStore.addEvent(event)
